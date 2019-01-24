35456497e7862961be6fa3ac8a19057bbdb60de4
nasum <nasu@scouty.co.jp>
2019-01-25 00:46:56 +0900

    show repo list

--- github_collector.py
+++ github_collector.py
@@ -1,10 +1,17 @@
+import os
 import fire
 
 from github import Github
+from github.Repository import Repository
+
 
 class GithubCollector(object):
-  def my_commit(self, start, end):
-    print(start)
-    
+
+    def my_commit(self, start=None, end=None):
+        g = Github(os.environ['GITHUB_ACCESS_TOKEN'])
+        for repo in g.get_user().get_repos(sort='pushed'):
+            print(repo.name)
+
+
 if __name__ == '__main__':
-  fire.Fire(GithubCollector)
+    fire.Fire(GithubCollector)
