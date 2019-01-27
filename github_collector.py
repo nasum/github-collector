891f8bbcc49443dcb8a68791865583a8fdb52f97
nasum <nasu@scouty.co.jp>
2019-01-27 23:20:36 +0900

    print commit url

--- github_collector.py
+++ github_collector.py
@@ -2,15 +2,16 @@ import os
 import fire
 
 from github import Github
-from github.Repository import Repository
 
 
 class GithubCollector(object):
 
     def my_commit(self, start=None, end=None):
         g = Github(os.environ['GITHUB_ACCESS_TOKEN'])
-        for repo in g.get_user().get_repos(sort='pushed'):
-            print(repo.name)
+        user = g.get_user()
+        for repo in user.get_repos(sort='pushed'):
+            for commit in repo.get_commits(author=user):
+                print(commit.html_url)
 
 
 if __name__ == '__main__':
